8d59786e687df8fcd0a48e818b43928f7803baa2
王建鹏 <jianpeng.wang@qingclass.com>
2019-07-29 16:38:51 +0800

    开发路由（处理POST Data）

--- node/app.js
+++ node/app.js
@@ -3,9 +3,34 @@ const querystring = require("querystring");
 const handleBlogRouter = require("./src/router/blog");
 const handleUserRouter = require("./src/router/user");
 
-const serverHandle = (req, res) => {
+// 处理postData
+const getPostData = req => {
+  return new Promise((resolve, reject) => {
+    if (req.method !== "POST") {
+      resolve({});
+      return;
+    }
+    if (req.headers["content-type"] !== "application/json") {
+      resolve({});
+      return;
+    }
+    let postData = "";
+    req.on("data", chunk => {
+      postData += chunk.toString();
+    });
+    req.on("end", () => {
+      if (!postData) {
+        resolve({});
+        return;
+      }
+      resolve(JSON.parse(postData));
+    });
+  });
+};
+
+const serverHandle = async (req, res) => {
   // 设置返回格式 JSON
-  res.setHeader("Content-type", "application/json");
+  res.setHeader("Content-Type", "application/json");
 
   // 获取 path
   const url = req.url;
@@ -13,6 +38,10 @@ const serverHandle = (req, res) => {
 
   // 解析 query
   req.query = querystring.parse(url.split("?")[1]);
+
+  // 处理 post data
+  const postData = await getPostData(req);
+  req.body = postData;
 
   const blogData = handleBlogRouter(req, res);
   if (blogData) {
@@ -27,7 +56,7 @@ const serverHandle = (req, res) => {
   }
 
   // 未命中路由，返回 404
-  res.writeHead(404, { "Content-type": "text/plain" });
+  res.writeHead(404, { "Content-Type": "text/plain" });
   res.write("404 Not Fount\n");
   res.end();
 };
