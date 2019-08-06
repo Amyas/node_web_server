abfbdcd3488091cb9643716bc362dd2b89b23bf0
王建鹏 <jianpeng.wang@qingclass.com>
2019-08-06 12:23:23 +0800

    koa2 初始化路由

--- koa2-blog/app.js
+++ koa2-blog/app.js
@@ -7,6 +7,8 @@ const bodyparser = require("koa-bodyparser");
 const logger = require("koa-logger");
 
 const index = require("./routes/index");
+const blog = require("./routes/blog");
+const user = require("./routes/user");
 
 // error handler
 onerror(app);
@@ -30,6 +32,8 @@ app.use(async (ctx, next) => {
 
 // routes
 app.use(index.routes(), index.allowedMethods());
+app.use(blog.routes(), blog.allowedMethods());
+app.use(user.routes(), user.allowedMethods());
 
 // error-handling
 app.on("error", (err, ctx) => {
