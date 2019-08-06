d1e7adc881ca4e15a7da252d4e302eca74b69ef6
王建鹏 <jianpeng.wang@qingclass.com>
2019-08-06 11:44:12 +0800

    开发路由

--- express-blog/routes/blog.js
+++ express-blog/routes/blog.js
@@ -9,10 +9,22 @@ const {
   removeBlog
 } = require("../controller/blog");
 const { SuccessModel, ErrorModel } = require("../model/resModel");
+const loginCheck = require("../middleware/loginCheck");
 
 router.get("/list", (req, res, next) => {
   let author = req.query.author || "";
   const keyword = req.query.keyword || "";
+
+  if (req.query.isadmin) {
+    // 管理员界面
+    if (!req.session.username) {
+      // 未登录
+      res.json(new ErrorModel("未登录"));
+      return;
+    }
+    // 强制查询自己的博客
+    author = req.session.username;
+  }
 
   return getList(author, keyword).then(data => {
     res.json(new SuccessModel(data));
@@ -20,9 +32,36 @@ router.get("/list", (req, res, next) => {
 });
 
 router.get("/detail", (req, res, next) => {
-  res.json({
-    errno: 0,
-    data: "ok"
+  return getDetail(req.query.id).then(data => {
+    res.json(new SuccessModel(data));
+  });
+});
+
+router.post("/new", loginCheck, (req, res, next) => {
+  req.body.author = req.session.username;
+  return createBlog(req.body).then(data => {
+    res.json(new SuccessModel(data));
+  });
+});
+
+router.post("/update", loginCheck, (req, res, next) => {
+  req.body.author = req.session.username;
+  return updateBlog(req.query.id, req.body).then(data => {
+    if (data) {
+      res.json(new SuccessModel("更新成功"));
+      return;
+    }
+    res.json(new ErrorModel("更新失败"));
+  });
+});
+
+router.post("/del", loginCheck, (req, res, next) => {
+  req.query.author = req.session.username;
+  return removeBlog(req.query.id, req.query.author).then(data => {
+    if (data) {
+      res.json(new SuccessModel("删除成功"));
+    }
+    res.json(new ErrorModel("删除失败"));
   });
 });
 
